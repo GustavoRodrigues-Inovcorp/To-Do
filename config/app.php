6744a3744728b1d4f1ab3e2d36828d23a225d831
Gustavo Rodrigues <gurodriguesalves12@gmail.com>
2026-05-05 13:12:31 +0100

    fix(production): use Render external URL for asset generation

--- config/app.php
+++ config/app.php
@@ -54,6 +54,8 @@ return [
 
     'url' => env('RENDER_EXTERNAL_URL', env('APP_URL', 'http://localhost')),
 
+    'asset_url' => env('RENDER_EXTERNAL_URL', env('ASSET_URL')),
+
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
